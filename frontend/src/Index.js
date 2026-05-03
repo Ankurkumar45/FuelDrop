dedc2f6c0863c106c95ce01f74d912656ae72227
Ankurkumar45 <ankurkumardwivedi779@gmail.com>
2026-05-03 15:20:17 +0530

    Enable order management by importing orderReducer in the Redux store configuration.

--- frontend/src/Index.js
+++ frontend/src/Index.js
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './store/authSlice';
 import pumpReducer from './store/pumpSlice';
-// import orderReducer from './store/orderSlice';
+import orderReducer from './store/orderSlice';
 // import sosReducer from './store/sosSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         pump: pumpReducer,
-        // order: orderReducer,
+        order: orderReducer,
         // sos: sosReducer
     },
 });
