6c9b13786e2f7529814d3773c53112035eb59afa
Ankur Kumar Dwivedi <ankurkumardwivedi779@gmail.com>
2026-05-11 10:13:56 +0530

    fix: correct import casing for store in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './index.js'
+import { store } from './Index.js'
 import React from 'react'
 
 createRoot(document.getElementById('root')).render(
